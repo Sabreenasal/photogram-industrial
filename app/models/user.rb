dc3320816f59510b6833c8fdf39a1d40d0e6d55b
Sabreena Salomon <salomonbree@gmail.com>
2025-05-22 18:38:57 +0000

    generated comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,5 +30,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"         
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"   
+  has_many :comments, foreign_key: "author_id"      
 end
