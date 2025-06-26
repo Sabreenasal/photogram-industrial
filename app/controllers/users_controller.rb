d54bd22a4217b3b6b50c21fbdbf0b88eb4dd2310
Sabreena Salomon <salomonbree@gmail.com>
2025-06-26 03:52:25 +0000

    added bootstrap to other views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index 
-    @user = User.all
+    @users = User.all
   end
     
     def show
     @user = User.find_by!(username: params.fetch(:username))
+
   end
   
   def liked
